51011c63b2639ceec99f1efd597fd33c7d32456a
Radhwene <kenzizirathwene64@gmail.com>
2026-03-07 21:53:50 +0100

    chore: initialisation du projet (dependances et structure generale)

---
+++ src/main/java/com/ooredoo/hr/attrition/predictor/HrAttritionPredictorApplication.java
@@ -0,0 +1,13 @@
+package com.ooredoo.hr.attrition.predictor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrAttritionPredictorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrAttritionPredictorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ooredoo/hr/attrition/predictor/HrAttritionPredictorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ooredoo.hr.attrition.predictor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrAttritionPredictorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
